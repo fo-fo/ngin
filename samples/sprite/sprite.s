6f12120958f99bf4510c68c4963a2154389b7dfe
f <f@aspekt.fi>
2015-03-01 16:20:18 +0200

    Added support for generic memory operations.

--- samples/sprite/sprite.s
+++ samples/sprite/sprite.s
@@ -1,54 +1,5 @@
 .include "ngin/ngin.inc"
 
-ngin_entryPoint start
-.proc start
-    jsr uploadPalette
-    jsr renderSprites
-
-    ngin_pollVBlank
-    ngin_mov8 ppu::oam::dma, #.hibyte( ngin_shadowOam )
-    ngin_mov8 ppu::mask, #( ppu::mask::kShowSprites | \
-                            ppu::mask::kShowSpritesLeft )
-
-    jmp *
-.endproc
-
-.proc uploadPalette
-    ngin_pollVBlank
-
-    ngin_setPpuAddress #ppu::spritePalette
-
-    ; \todo Symbolic constants for color hues? (separate hue and brightness)
-    ngin_mov8 ppu::data, #$F
-    lda #$2B
-    .repeat 3
-        sta ppu::data
-    .endrepeat
-
-    ngin_mov8 ppu::data, #$F
-    lda #$38
-    .repeat 3
-        sta ppu::data
-    .endrepeat
-
-    rts
-.endproc
-
-.proc renderSprites
-    ngin_ShadowOam_startFrame
-
-    ngin_SpriteRenderer_render #spriteDefinition, \
-                               #ngin_immediateVector2_16 0, 0
-    ngin_SpriteRenderer_render #spriteDefinition, \
-                               #ngin_immediateVector2_16 -65, 50
-
-    ngin_ShadowOam_endFrame
-
-    rts
-.endproc
-
-; -----------------------------------------------------------------------------
-
 .segment "RODATA"
 
 spriteDefinition:
@@ -76,6 +27,59 @@ spriteDefinition:
     .byte 1                                         ; Tile
 
     .byte ngin_kSpriteDefinitionTerminator
+
+.proc spritePalette
+    ; \todo Symbolic constants for color hues? (separate hue and brightness)
+    ;       E.g. ngin_Color::black
+    kBackgroundColor = $F
+    .byte kBackgroundColor, $2B, $2B, $2B
+    .byte kBackgroundColor, $38, $38, $38
+    .byte kBackgroundColor, $0F, $0F, $0F
+    .byte kBackgroundColor, $0F, $0F, $0F
+.endproc
+
+; -----------------------------------------------------------------------------
+
+ngin_entryPoint start
+.proc start
+    jsr uploadPalette
+    jsr renderSprites
+
+    ngin_pollVBlank
+    ngin_mov8 ppu::oam::dma, #.hibyte( ngin_shadowOam )
+    ngin_mov8 ppu::mask, #( ppu::mask::kShowSprites | \
+                            ppu::mask::kShowSpritesLeft )
+
+    jmp *
+.endproc
+
+.proc uploadPalette
+    ngin_pollVBlank
+
+    ; Set all palettes to black.
+    ngin_setPpuAddress #ppu::backgroundPalette
+    ngin_fillPort #ppu::data, #$F, #32
+
+    ; Upload sprite palette.
+    ngin_setPpuAddress #ppu::spritePalette
+    ; \note .sizeof cannot be used with imported symbols
+    ngin_copyMemoryToPort #ppu::data, #spritePalette, #.sizeof( spritePalette )
+
+    rts
+.endproc
+
+.proc renderSprites
+    ngin_ShadowOam_startFrame
+
+    ngin_SpriteRenderer_render #spriteDefinition, \
+                               #ngin_immediateVector2_16 0, 0
+    ngin_SpriteRenderer_render #spriteDefinition, \
+                               #ngin_immediateVector2_16 -65, 50
+
+    ngin_ShadowOam_endFrame
+
+    rts
+.endproc
 
 ; -----------------------------------------------------------------------------
 
