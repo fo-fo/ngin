8295d0541ce8468ee2065d9fe58ec3255b79338b
f <f@aspekt.fi>
2015-05-05 15:48:01 +0300

    Build log macros output the strings directly to the correct segment. Added more segment sizes to the Ngin build log.

--- src/ngin/build-log.s
+++ src/ngin/build-log.s
@@ -2,24 +2,37 @@
 
 .export __ngin_BuildLog_forceImport : absolute = 1
 
+.macro printSegmentSize segment
+    .define __sizeIdentifier .ident( .sprintf( "__%s_SIZE__", segment ) )
+    .import __sizeIdentifier
+    __ngin_BuildLog_string .sprintf( "  %25s", segment )
+    __ngin_BuildLog_decimalInteger __sizeIdentifier
+    __ngin_BuildLog_string " bytes"
+    __ngin_BuildLog_newLine
+    .undefine __sizeIdentifier
+.endmacro
+
 .segment "NGIN_BUILD_LOG_HEADER"
-    ngin_BuildLog_string "Ngin Build Log"
-    ngin_BuildLog_newLine
-    ngin_BuildLog_string "--------------"
-    ngin_BuildLog_newLine
-    ngin_BuildLog_newLine
+    __ngin_BuildLog_string "Ngin Build Log"
+    __ngin_BuildLog_newLine
+    __ngin_BuildLog_string "--------------"
+    __ngin_BuildLog_newLine
+    __ngin_BuildLog_newLine
 
 .segment "NGIN_BUILD_LOG"
-    ngin_BuildLog_string "NGIN_CODE segment size:"
-    ngin_BuildLog_newLine
-    .import __NGIN_CODE_SIZE__
-    ngin_BuildLog_decimalInteger __NGIN_CODE_SIZE__
-    ngin_BuildLog_string " bytes"
-    ngin_BuildLog_newLine
+    __ngin_BuildLog_string "Ngin Segments:"
+    __ngin_BuildLog_newLine
+    printSegmentSize "NGIN_CODE"
+    printSegmentSize "NGIN_RESET_PROLOGUE"
+    printSegmentSize "NGIN_RESET_CONSTRUCTORS"
+    printSegmentSize "NGIN_RESET_EPILOGUE"
+    printSegmentSize "NGIN_RODATA"
+    printSegmentSize "NGIN_BSS"
+    __ngin_BuildLog_newLine
 
-    ngin_BuildLog_string "CODE segment size:"
-    ngin_BuildLog_newLine
-    .import __CODE_SIZE__
-    ngin_BuildLog_decimalInteger __CODE_SIZE__
-    ngin_BuildLog_string " bytes"
-    ngin_BuildLog_newLine
+    __ngin_BuildLog_string "User Segments:"
+    __ngin_BuildLog_newLine
+    printSegmentSize "CODE"
+    printSegmentSize "RODATA"
+    printSegmentSize "BSS"
+    __ngin_BuildLog_newLine
