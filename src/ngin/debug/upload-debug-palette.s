42a09b305757af4d371755cb4cf9ba0c3d4b649e
f <f@aspekt.fi>
2015-05-16 21:38:59 +0300

    Added Debug module, and ngin_Debug_uploadDebugPalette macro that can be used to easily set up a test palette.

---
+++ src/ngin/debug/upload-debug-palette.s
@@ -0,0 +1,29 @@
+.include "ngin/debug.inc"
+.include "ngin/ppu.inc"
+.include "ngin/memory.inc"
+
+.segment "NGIN_CODE"
+
+.proc __ngin_Debug_uploadDebugPalette
+    ngin_pushSeg "NGIN_RODATA"
+    .proc palette
+        .byte $0F, $06, $16, $26
+        .byte $0F, $09, $19, $29
+        .byte $0F, $02, $12, $22
+        .byte $0F, $04, $14, $24
+
+        .byte $0F, $07, $17, $27
+        .byte $0F, $0B, $1B, $2B
+        .byte $0F, $03, $13, $23
+        .byte $0F, $05, $15, $25
+    .endproc
+    ngin_popSeg
+
+    ; Avoid palette artifacts by polling for vblank.
+    ngin_pollVBlank
+
+    ngin_setPpuAddress #ppu::backgroundPalette
+    ngin_copyMemoryToPort #ppu::data, #palette, #.sizeof( palette )
+
+    rts
+.endproc
