d1a7606e9d805d08bd6996d4c1bab8e66d09e25b
f <f@aspekt.fi>
2015-05-16 23:25:34 +0300

    Added SpriteAnimator module, which can be used to playback sprite animations. Added a sample for it.

---
+++ samples/sprite-animation/sprite-animation.s
@@ -0,0 +1,36 @@
+.include "ngin/ngin.inc"
+
+; From asset importer:
+.include "sprites.inc"
+
+ngin_bss animationState: .tag ngin_SpriteAnimator_State
+
+.segment "CODE"
+
+ngin_entryPoint start
+.proc start
+    ngin_Debug_uploadDebugPalette
+
+    ; Enable NMI so that we can use ngin_waitVBlank.
+    ngin_mov8 ppu::ctrl, #ppu::ctrl::kGenerateVblankNmi
+
+    ngin_SpriteAnimator_initialize animationState, #animation_ryu
+
+    loop:
+        ngin_ShadowOam_startFrame
+
+        ngin_SpriteRenderer_render \
+            animationState + ngin_SpriteAnimator_State::spriteDefinition, \
+            #ngin_immediateVector2_16 ngin_SpriteRenderer_kTopLeftX+256/2, \
+                                      ngin_SpriteRenderer_kTopLeftY+240/2
+
+        ngin_SpriteAnimator_update animationState
+
+        ngin_ShadowOam_endFrame
+
+        ngin_waitVBlank
+        ngin_mov8 ppu::oam::dma, #.hibyte( ngin_ShadowOam_buffer )
+        ngin_mov8 ppu::mask, #( ppu::mask::kShowSprites | \
+                                ppu::mask::kShowSpritesLeft )
+    jmp loop
+.endproc
---
+++ src/ngin/sprite-animator.s
@@ -0,0 +1,53 @@
+.include "ngin/sprite-animator.inc"
+.include "ngin/sprite-renderer.inc"
+.include "ngin/branch.inc"
+.include "ngin/lua/lua.inc" ; \todo Remove!
+
+.segment "NGIN_ZEROPAGE" : zeropage
+
+__ngin_SpriteAnimator_update_state: .tag ngin_SpriteAnimator_State
+
+.segment "NGIN_CODE"
+
+.proc __ngin_SpriteAnimator_update
+    state := __ngin_SpriteAnimator_update_state
+
+    ; -------------------------------------------------------------------------
+    ; \note This function has to preserve X and Y. See the macro.
+    ; -------------------------------------------------------------------------
+
+    ; Decrease the amount of delay left, and when 0, grab the next frame.
+    ; \todo This decrease could be moved into the macro to avoid the copy,
+    ;       since most of the time will be spent waiting for the delay to
+    ;       elapse (\note DEC doesn't work in all addressing modes)
+    dec state + ngin_SpriteAnimator_State::delayLeft
+    ngin_branchIfZero nextFrame
+        rts
+    nextFrame:
+
+    ; Preserve Y. Reuse delayLeft as temporary storage, since we know its state,
+    ; and it will be overwritten later.
+    preservedY := state + ngin_SpriteAnimator_State::delayLeft
+    sty preservedY
+
+    ; Fetch the next frame by following the link in the current frame.
+    ldy #ngin_SpriteRenderer_Header::next
+    lda ( state + ngin_SpriteAnimator_State::spriteDefinition ), y
+    ; Store in a temporary variable, because we can't modify spriteDefinition
+    ; before reading the next byte from there.
+    pha
+    iny
+    lda ( state + ngin_SpriteAnimator_State::spriteDefinition ), y
+    sta state + ngin_SpriteAnimator_State::spriteDefinition + 1
+    pla
+    sta state + ngin_SpriteAnimator_State::spriteDefinition + 0
+
+    ; Now read the delay from the *new* sprite definition.
+    ldy #ngin_SpriteRenderer_Header::delay
+    lda ( state + ngin_SpriteAnimator_State::spriteDefinition ), y
+    ; Restore Y.
+    ldy preservedY
+    sta state + ngin_SpriteAnimator_State::delayLeft
+
+    rts
+.endproc
