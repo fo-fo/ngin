bfdd5c5632b6856b8efc0ebaf0513865c5b12acb
f <f@aspekt.fi>
2015-06-03 13:53:45 +0300

    Switched topLeft -> leftTop in collision routine to match the order of components in memory.

--- src/ngin/collision.s
+++ src/ngin/collision.s
@@ -5,41 +5,41 @@
 
 .segment "NGIN_BSS"
 
-__ngin_Collision_rectOverlap_rectATopLeft:      .tag ngin_Vector2_16
-__ngin_Collision_rectOverlap_rectABottomRight:  .tag ngin_Vector2_16
-__ngin_Collision_rectOverlap_rectBTopLeft:      .tag ngin_Vector2_16
-__ngin_Collision_rectOverlap_rectBBottomRight:  .tag ngin_Vector2_16
+__ngin_Collision_rectOverlap_rectALeftTop:      .tag ngin_Vector2_16
+__ngin_Collision_rectOverlap_rectARightBottom:  .tag ngin_Vector2_16
+__ngin_Collision_rectOverlap_rectBLeftTop:      .tag ngin_Vector2_16
+__ngin_Collision_rectOverlap_rectBRightBottom:  .tag ngin_Vector2_16
 
 .segment "NGIN_CODE"
 
 .proc __ngin_Collision_rectOverlap
-    rectATopLeft     := __ngin_Collision_rectOverlap_rectATopLeft
-    rectABottomRight := __ngin_Collision_rectOverlap_rectABottomRight
-    rectBTopLeft     := __ngin_Collision_rectOverlap_rectBTopLeft
-    rectBBottomRight := __ngin_Collision_rectOverlap_rectBBottomRight
+    rectALeftTop     := __ngin_Collision_rectOverlap_rectALeftTop
+    rectARightBottom := __ngin_Collision_rectOverlap_rectARightBottom
+    rectBLeftTop     := __ngin_Collision_rectOverlap_rectBLeftTop
+    rectBRightBottom := __ngin_Collision_rectOverlap_rectBRightBottom
 
     ; All of following must be true for the rects to overlap:
-    ;     rectATopLeft.X     < rectBBottomRight.X  and
-    ;     rectABottomRight.X > rectBTopLeft.X      and
-    ;     rectATopLeft.Y     < rectBBottomRight.Y  and
-    ;     rectABottomRight.Y > rectBTopLeft.Y
+    ;     rectALeftTop.X     < rectBRightBottom.X  and
+    ;     rectARightBottom.X > rectBLeftTop.X      and
+    ;     rectALeftTop.Y     < rectBRightBottom.Y  and
+    ;     rectARightBottom.Y > rectBLeftTop.Y
 
     ; \todo The horizontal and vertical case can be macroified.
-    ngin_cmp16 rectATopLeft     + ngin_Vector2_16::x_, \
-               rectBBottomRight + ngin_Vector2_16::x_
+    ngin_cmp16 rectALeftTop     + ngin_Vector2_16::x_, \
+               rectBRightBottom + ngin_Vector2_16::x_
     ngin_branchIfGreaterOrEqual noOverlapClearCarry
 
-    ngin_cmp16 rectABottomRight + ngin_Vector2_16::x_, \
-               rectBTopLeft     + ngin_Vector2_16::x_
+    ngin_cmp16 rectARightBottom + ngin_Vector2_16::x_, \
+               rectBLeftTop     + ngin_Vector2_16::x_
     ngin_branchIfLess noOverlapRts
     beq noOverlapClearCarry
 
-    ngin_cmp16 rectATopLeft     + ngin_Vector2_16::y_, \
-               rectBBottomRight + ngin_Vector2_16::y_
+    ngin_cmp16 rectALeftTop     + ngin_Vector2_16::y_, \
+               rectBRightBottom + ngin_Vector2_16::y_
     ngin_branchIfGreaterOrEqual noOverlapClearCarry
 
-    ngin_cmp16 rectABottomRight + ngin_Vector2_16::y_, \
-               rectBTopLeft     + ngin_Vector2_16::y_
+    ngin_cmp16 rectARightBottom + ngin_Vector2_16::y_, \
+               rectBLeftTop     + ngin_Vector2_16::y_
     ngin_branchIfLess noOverlapRts
     ; If this branch is taken, rects overlap, and carry is already 1.
     bne overlap
