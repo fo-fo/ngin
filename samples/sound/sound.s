d6f13285ec2a57362a62f90c004ad36a43885b15
f <f@aspekt.fi>
2015-08-09 17:53:41 +0300

    Change songs / trigger sound effects with buttons in the sound sample.

--- samples/sound/sound.s
+++ samples/sound/sound.s
@@ -1,8 +1,6 @@
-.include "ngin/ngin.inc"
+; Press A/B/Up/Right to play different songs and sound effects.
 
-; \todo More songs
-; \todo Trigger sound effects with controller
-; \todo Switch songs with controller
+.include "ngin/ngin.inc"
 
 ; From asset importer:
 .include "sounds.inc"
@@ -15,33 +13,48 @@ ngin_entryPoint start
     ngin_mov8 ppu::ctrl, #ppu::ctrl::kGenerateVblankNmi
 
     ngin_Muse_init #sounds
-    ; ngin_Muse_startSong #sounds::songs::test_song
     ngin_Muse_startSong #sounds::songs::ninja_song
 
     ; Unpause the music.
     ; \todo Expose flags, or expose pause/etc as macros.
     ngin_Muse_setFlags #0
 
-    kSoundEffectRate = 50
-    ngin_bss counter: .byte 0
-    ngin_mov8 counter, #kSoundEffectRate
     loop:
-        dec counter
-        ngin_branchIfNotZero counterNotZero
-            ngin_mov8 counter, #kSoundEffectRate
-            ngin_Lfsr8_random
-            pha
-            and #%1
-            ngin_branchIfZero dontTrigger
-                ngin_Muse_startEffect #sounds::effects::checkpoint, #0
-            dontTrigger:
+        ngin_bss previousController: .byte 0
+        ngin_mov8 previousController, controller
+        ngin_Controller_read1
+        ngin_bss controller: .byte 0
+        sta controller
 
-            pla
-            and #%10
-            ngin_branchIfZero dontTrigger2
-                ngin_Muse_startEffect #sounds::effects::break, #1
-            dontTrigger2:
-        counterNotZero:
+        lda previousController
+        eor #$FF
+        and controller
+        ngin_bss newController: .byte 0
+        sta newController
+
+        lda newController
+        and #ngin_Controller::kA
+        ngin_branchIfZero notA
+            ngin_Muse_startSong #sounds::songs::ninja_song
+        notA:
+
+        lda newController
+        and #ngin_Controller::kB
+        ngin_branchIfZero notB
+            ngin_Muse_startSong #sounds::songs::castle_song
+        notB:
+
+        lda newController
+        and #ngin_Controller::kRight
+        ngin_branchIfZero notRight
+            ngin_Muse_startEffect #sounds::effects::checkpoint, #0
+        notRight:
+
+        lda newController
+        and #ngin_Controller::kUp
+        ngin_branchIfZero notUp
+            ngin_Muse_startEffect #sounds::effects::break, #1
+        notUp:
 
         ngin_Nmi_waitVBlank
         ngin_Muse_update
