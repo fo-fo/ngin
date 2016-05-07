8acc4dc80eaaee84b123d7df3f7080e9cb14d23d
f <f@aspekt.fi>
2016-05-08 00:54:06 +0300

    A coordinate offset can be specified for sprite rendering in configuration.

--- src/ngin/config.in.lua
+++ src/ngin/config.in.lua
@@ -5,3 +5,6 @@ ngin_cfg_MapData_viewWidth      = ${ngin_cfg_MapData_viewWidth}
 ngin_cfg_MapData_viewHeight     = ${ngin_cfg_MapData_viewHeight}
 ngin_cfg_MapData_attrViewWidth  = ${ngin_cfg_MapData_attrViewWidth}
 ngin_cfg_MapData_attrViewHeight = ${ngin_cfg_MapData_attrViewHeight}
+
+ngin_cfg_SpriteRenderer_offsetX = ${ngin_cfg_SpriteRenderer_offsetX}
+ngin_cfg_SpriteRenderer_offsetY = ${ngin_cfg_SpriteRenderer_offsetY}
